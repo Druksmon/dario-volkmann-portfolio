334645eba7a443b2c1d2cd8bde078c9a55e28bcb
Druks <dariusvolkmann@gmail.com>
2022-09-22 16:51:42 -0300

    add firebase icon

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -76,14 +76,12 @@ const Skills = () => {
                         </motion.div>
                         <motion.div whileHover={{scale: 1.1}} transition={{duration: 0.5, type: "spring"}}
                                     className="skills-icons">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="53.324" height="53" viewBox="0 0 16 16"
-                                 fill="currentColor" className="bi bi-terminal">
+                            <svg  width="53.324" height="53" viewBox="0 0 24 24"
+                                 xmlns="http://www.w3.org/2000/svg"><title>Firebase icon</title>
                                 <path
-                                    d="M6 9a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3A.5.5 0 0 1 6 9zM3.854 4.146a.5.5 0 1 0-.708.708L4.793 6.5 3.146 8.146a.5.5 0 1 0 .708.708l2-2a.5.5 0 0 0 0-.708l-2-2z"/>
-                                <path
-                                    d="M2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h12z"/>
+                                    d="M3.89 15.673L6.255.461A.542.542 0 0 1 7.27.289L9.813 5.06 3.89 15.673zm16.795 3.691L18.433 5.365a.543.543 0 0 0-.918-.295l-14.2 14.294 7.857 4.428a1.62 1.62 0 0 0 1.587 0l7.926-4.428zM14.3 7.148l-1.82-3.482a.542.542 0 0 0-.96 0L3.53 17.984 14.3 7.148z"/>
                             </svg>
-                            <h5>TERMINAL</h5>
+                            <h5>FIREBASE</h5>
                         </motion.div>
                         <motion.div whileHover={{scale: 1.1}} transition={{duration: 0.5, type: "spring"}}
                                     className="skills-icons">
